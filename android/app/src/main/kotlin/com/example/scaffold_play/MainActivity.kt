016e00458e87d5cf437677264cb1e75be0af8f23
eoeefosa <eoeefosa@gmail.com>
2022-07-11 11:13:19 +0100

    created a scafold project to see scaffold widget

---
+++ android/app/src/main/kotlin/com/example/scaffold_play/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.scaffold_play
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // final theme = FooderlichTheme.light();
+    return MaterialApp();
+  }
+}
